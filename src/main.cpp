a2c69a85771f89cbe40f02c15e2098cade96b8fd
ccn <ccn@cuppajoeman.com>
2025-12-06 18:17:05 -0500

    feat: improving handling of multiple mapped keys at once

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 #include "utility/collection_utils/collection_utils.hpp"
 #include "utility/logger/logger.hpp"
+#include "utility/temporal_binary_switch/temporal_binary_switch.hpp"
 #include "utility/timer/timer.hpp"
 
 #include <iostream>
@@ -77,6 +78,55 @@ class KeyInterceptor {
     }
 };
 
+/**
+ *
+ * Motivation:
+ *
+ * vim taught us that we can avoid reaching for our mouse. Once mastered, we should continue this pattern in memory of
+ * vim. Moreover if you are a programmer or someone who usese the computer for a lot of time then you should be
+ * investing in the ability to continue doing this. If one day you got a repetitive strain injury and were no longer
+ * able to type anymore it would be a sad day.
+ *
+ * Ok so applying the same reasoning that moving your hand is bad, then we just have to see how we currently move our
+ * hand. Most vimmers already rebind escape to caps lock because they understand this concept, but thats usually where
+ * it ends. Let's not stop there, instead we should realize that anytime we have to move away from the homerow this is
+ * usually a hand movement, the worst offenders are those where we have to slightly adjust the hand to reach our pinky
+ * out to grab a key, such as caps lock, delete, tab, enter etc.
+ *
+ * Our solution to this problem is to move this keys inward so that no hand adjustments have to made. This is a sort of
+ * mapping layer, but how do we activate this mapping layer?
+ *
+ * Clearly we want to adhere to the principle of not moving your hand and so we don't have the option of enabling this
+ * mapping mode with traditional keys like ctrl/alt/super etc so instead we have to try something new.
+ *
+ * Space is the biggest key on the keyboard and pressed by the strongest finger on your hand, so leveraging this key
+ * would be good, simply mapping space to activate the mapping mode would be bad, because we use space for other thing
+ * as well.
+ *
+ * One area that we can take advantage of is timing. If we get the user to press space in fast succession we'll enable
+ * the mapping layer, if the second space is not emitted within the time frame then we can the mode is not activated,
+ * and if you press any key other than space after hitting space, then the mod will also not be activated. This keeps
+ * the regular behavior while adding the mapping layer if you know the timing and go fast.
+ *
+ * TLDR:
+ *
+ * space tap, space hold in fast succession activates the mapping mode, in this mode, certain keys are remapped to other
+ * keys
+ *
+ * when space is released the mapping mode turns off and keys go back to their regular function unless the following is
+ * true
+ *
+ * if a key is remapped by the mode and it is continually held down even when space is released then it continues to
+ * repeat the mapped key.
+ *
+ * the purpose of this extra functionality is to allow you to combine mapped and unmapped keys, so for example if / is
+ * mapped to right shift in the mapped state and a is mapped to escape in the mapped state, then it's impossible to type
+ * shift-a in the mapped state because a is already being mapped to something. In order to allow for such situation we
+ * need to be able to conditionally keep mapped keys active, and to do this we use above method. In this way we can
+ * activate the mapping mode, and then hold down / to keep shift active
+ *
+ */
+// TODO: this needs to be renamed, and then the one with these specific mappings is the homebody keyboard mappings
 class ChordSystem {
 
   public:
@@ -98,15 +148,17 @@ class ChordSystem {
     KeyInterceptor key_interceptor;
 
     bool timer_started_at_least_once = false;
-    Timer chord_activation_timer{0.2};
+    Timer mapping_mode_activation_timer{0.2};
 
-    bool chord_started = false;
+    bool mapping_mode_active = false;
 
-    bool first_space_pressed = false;
+    bool possibly_going_into_mapping_mode = false;
 
     struct ChordMapping {
         EKey input_key;
         EKey output_key;
+        bool active = false;
+        TemporalBinarySwitch active_tbs;
     };
 
     std::vector<ChordMapping> chord_mappings;
@@ -120,20 +172,14 @@ class ChordSystem {
         global_logger->debug("space signal state: {}",
                              input_state.key_enum_to_object.at(EKey::SPACE)->pressed_signal.to_string());
 
-        std::cout << "0" << std::endl;
         if (input_state.is_just_pressed(EKey::SPACE)) {
-            std::cout << "1" << std::endl;
-            std::cout << "time up: " << chord_activation_timer.time_up()
-                      << " not tsalo: " << not timer_started_at_least_once << std::endl;
-            if (chord_activation_timer.time_up() or not timer_started_at_least_once) {
-                chord_started = false;
-                std::cout << "2" << std::endl;
-                chord_activation_timer.start();
-                first_space_pressed = true;
+            if (mapping_mode_activation_timer.time_up() or not timer_started_at_least_once) {
+                mapping_mode_active = false;
+                mapping_mode_activation_timer.start();
+                possibly_going_into_mapping_mode = true;
                 timer_started_at_least_once = true;
             } else { // the timer was not up
-                std::cout << "3" << std::endl;
-                chord_started = true;
+                mapping_mode_active = true;
                 global_logger->debug("chord started");
             }
             // If you manually press space, it gets ignored
@@ -142,43 +188,20 @@ class ChordSystem {
 
         // only if the time for the chord to start elapsed and you had pressed space we do a slightly delayed space
         // emission
-        if (not chord_started and chord_activation_timer.time_up() and first_space_pressed) {
-            std::cout << "space generated from delay" << std::endl;
+        if (not mapping_mode_active and mapping_mode_activation_timer.time_up() and possibly_going_into_mapping_mode) {
             send_key(key_interceptor.ufd, key_interceptor.key_enum_to_linux_code.at(EKey::SPACE),
                      LinuxInputAdapter::press_value);
             send_key(key_interceptor.ufd, key_interceptor.key_enum_to_linux_code.at(EKey::SPACE),
                      LinuxInputAdapter::release_value);
-            first_space_pressed = false;
-        }
-
-        // chord ends here
-        if (input_state.is_just_released(EKey::SPACE) and chord_started) {
-            chord_started = false;
-            // turn off all possible output keys from the chord mapping so they don't repeat if they were held down
-            // when space was released.
-            for (const auto &cm : chord_mappings) {
-                send_key(key_interceptor.ufd, key_interceptor.key_enum_to_linux_code.at(cm.output_key),
-                         LinuxInputAdapter::release_value);
-            }
-        }
-
-        auto just_pressed_keys = input_state.get_just_pressed_keys();
-        bool used_non_space_key =
-            not collection_utils::contains(just_pressed_keys, EKey::SPACE) and not just_pressed_keys.empty();
-        if (not chord_started and used_non_space_key and first_space_pressed) {
-            std::cout << "space generated from <space><nonspace>" << std::endl;
-            send_key(key_interceptor.ufd, key_interceptor.key_enum_to_linux_code.at(EKey::SPACE),
-                     LinuxInputAdapter::press_value);
-            send_key(key_interceptor.ufd, key_interceptor.key_enum_to_linux_code.at(EKey::SPACE),
-                     LinuxInputAdapter::release_value);
-            first_space_pressed = false;
+            // you took too long so we're longer trying to
+            possibly_going_into_mapping_mode = false;
         }
 
         // TODO: this doesn't work because it needs to not be reset per iteration because it doesn't have any effect
         // because it cannot effect more than one iteration and chord keys come through on different iterations
         int num_consecutive_keys_to_modify = 1;
 
-        auto chord_mapping = [&](EKey input_key, EKey output_key) {
+        auto transform_input_key_to_output_key = [&](EKey input_key, EKey output_key) {
             // NOTE: these if statements are mutually exclusive
 
             if (num_consecutive_keys_to_modify == 0)
@@ -187,7 +210,7 @@ class ChordSystem {
             if (input_state.is_just_pressed(input_key)) {
                 send_key(key_interceptor.ufd, key_interceptor.key_enum_to_linux_code.at(output_key),
                          LinuxInputAdapter::press_value);
-                first_space_pressed = false;
+                possibly_going_into_mapping_mode = false;
                 num_consecutive_keys_to_modify--;
             }
             if (input_state.is_held(input_key)) {
@@ -206,11 +229,73 @@ class ChordSystem {
             key_interceptor.keys_to_ignore_this_update.push_back(input_key);
         };
 
-        // TODO: generalize with more stuff later
-        if (chord_started) {
-            for (const auto &cm : chord_mappings) {
-                chord_mapping(cm.input_key, cm.output_key);
+        // chord ends here
+        if (input_state.is_just_released(EKey::SPACE) and mapping_mode_active) {
+            mapping_mode_active = false;
+            // turn off all possible output keys from the chord mapping so they don't repeat if they were held down
+            // when space was released.
+            for (auto &cm : chord_mappings) {
+                // leave actively pressed keys on.
+                if (input_state.is_pressed(cm.input_key))
+                    continue;
+
+                // release all other keys
+                cm.active = false;
+
+                send_key(key_interceptor.ufd, key_interceptor.key_enum_to_linux_code.at(cm.output_key),
+                         LinuxInputAdapter::release_value);
             }
+        }
+
+        auto just_pressed_keys = input_state.get_just_pressed_keys();
+        bool used_non_space_key =
+            not collection_utils::contains(just_pressed_keys, EKey::SPACE) and not just_pressed_keys.empty();
+        // when you type somethign like  "<space>a" we immediately emit the space key before this key so that you can
+        // type at full speed.
+        if (not mapping_mode_active and used_non_space_key and possibly_going_into_mapping_mode) {
+            send_key(key_interceptor.ufd, key_interceptor.key_enum_to_linux_code.at(EKey::SPACE),
+                     LinuxInputAdapter::press_value);
+            send_key(key_interceptor.ufd, key_interceptor.key_enum_to_linux_code.at(EKey::SPACE),
+                     LinuxInputAdapter::release_value);
+            possibly_going_into_mapping_mode = false;
+        }
+
+        // TODO: generalize with more stuff later
+        if (mapping_mode_active) {
+            for (auto &cm : chord_mappings) {
+                cm.active = true;
+                // transform_input_key_to_output_key(cm.input_key, cm.output_key);
+            }
+        }
+
+        // this does the mappings
+        for (auto &cm : chord_mappings) {
+
+            if (not cm.active)
+                continue;
+
+            int value;
+            switch (input_state.get_current_state(cm.input_key)) {
+            case TemporalBinarySwitch::State::just_switched_on:
+                possibly_going_into_mapping_mode = false; // WTF WHAT IS THAT
+                value = LinuxInputAdapter::press_value;
+                break;
+            case TemporalBinarySwitch::State::sustained_on:
+                value = LinuxInputAdapter::repeat_value;
+                break;
+            case TemporalBinarySwitch::State::just_switched_off:
+                value = LinuxInputAdapter::release_value;
+                cm.active = false;
+                break;
+            case TemporalBinarySwitch::State::sustained_off:
+                // doesn't need to be modeled by exclusion
+                continue; // we don't even send akey in this case
+                break;
+            }
+
+            key_interceptor.keys_to_ignore_this_update.push_back(cm.input_key);
+            send_key(key_interceptor.ufd, key_interceptor.key_enum_to_linux_code.at(cm.output_key), value);
+            // transform_input_key_to_output_key(cm.input_key, cm.output_key);
         }
     }
 };
